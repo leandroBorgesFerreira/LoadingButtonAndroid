197c51377593b8fbc7f443c81fc723647db5d270
leandro <lehen01@gmail.com>
2016-08-29 21:01:30 -0300

    sample added

--- app/src/main/java/br/com/simplepass/loadingbutton/MainActivity.java
+++ app/src/main/java/br/com/simplepass/loadingbutton/MainActivity.java
@@ -1,42 +1,52 @@
 package br.com.simplepass.loadingbutton;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import br.com.simplepass.loading_button_lib.CircularProgressButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Boolean isLoading;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        isLoading = false;
+        final CircularProgressButton progressButton = (CircularProgressButton) findViewById(R.id.progress_btn);
 
-        final CircularProgressButton progressEditText = (CircularProgressButton) findViewById(R.id.progress_edit_text);
-
-        progressEditText.setOnClickListener(new View.OnClickListener() {
+        progressButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!isLoading) {
-                    progressEditText.startAnimation();
-                    isLoading = true;
-                    Log.d("ET", "isLoading: " + isLoading.toString());
-                } else{
-                    progressEditText.stopAnimation();
-                    progressEditText.revertAnimation();
-                    isLoading = false;
+                animateButton(progressButton);
+            }
+        });
 
-                    Log.d("ET", "isLoading: " + isLoading.toString());
-                }
+        final CircularProgressButton progressButtonNoPadding =
+                (CircularProgressButton) findViewById(R.id.progress_btn_no_padding);
+
+        progressButtonNoPadding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateButton(progressButtonNoPadding);
             }
         });
     }
 
 
+    private void animateButton(final CircularProgressButton circularProgressButton){
+        Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                circularProgressButton.stopAnimation();
+                circularProgressButton.revertAnimation();
+            }
+        };
+
+        circularProgressButton.startAnimation();
+        handler.postDelayed(runnable, 3000);
+    }
 }
