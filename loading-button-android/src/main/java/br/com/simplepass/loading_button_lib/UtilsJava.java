202072f36193b81b287ff38636e2c34391f48739
JaiselRahman <jaiselrahman@gmailcom>
2018-05-19 20:38:52 +0530

    Fixed deprecated getDrawable(int)

--- loading-button-android/src/main/java/br/com/simplepass/loading_button_lib/UtilsJava.java
+++ loading-button-android/src/main/java/br/com/simplepass/loading_button_lib/UtilsJava.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,12 +15,7 @@ import android.widget.TextView;
 
 public class UtilsJava {
     public static Drawable getDrawable(Context context, int id){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return context.getDrawable(id);
-        } else {
-            return context.getResources().getDrawable(id);
-        }
-
+        return ContextCompat.getDrawable(context, id);
     }
 
     public static void setViewBackground(View view, Drawable drawable) {
