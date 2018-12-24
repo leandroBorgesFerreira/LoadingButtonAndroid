c140e879d4cb1bf2821c2281fff261ee8b96628b
Leandro Borges Ferreira <lehen01@gmail.com>
2018-12-24 18:18:25 -0200

    progress button - logic

---
+++ loading-button-android/src/main/java/br/com/simplepass/loading_button_lib/customViews/CircularProgressButton2.kt
@@ -0,0 +1,268 @@
+package br.com.simplepass.loading_button_lib.customViews
+
+import android.animation.*
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.*
+import android.os.Build
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
+import br.com.simplepass.loading_button_lib.animatedDrawables.CircularAnimatedDrawable
+import br.com.simplepass.loading_button_lib.animatedDrawables.CircularRevealAnimatedDrawable
+import br.com.simplepass.loading_button_lib.disposeAnimator
+
+private enum class State {
+    IDLE, MORPHING, MORPHING_REVERT, PROGRESS, DONE, STOPPED
+}
+
+class CircularProgressButton2 : AppCompatButton {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private var state: State = State.IDLE
+
+    //TODO: Fix this!
+    private val finalCorner = 0F
+    private val initialCorner = 0F
+    //TODO: Fix this!
+    private val drawable: GradientDrawable? = null
+
+
+    //TODO: Fix this!
+    private val finalWidth = 0
+    private val initialWidth = 0
+
+    //TODO: Fix this!
+    private val finalHeight = 0
+    private val initialHeight = 0
+
+    private var waitingToStartDone: Boolean = false
+
+    private var viewReady = false
+    private var waitingToStartProgress = false
+
+    private val initialText = text //Todo: Check if the text is not null!
+
+    private var progressAnimatedDrawable: CircularAnimatedDrawable? = null
+    private var revealAnimatedDrawable: CircularRevealAnimatedDrawable? = null
+
+    private val morphAnimator by lazy {
+        AnimatorSet().apply {
+            playTogether(
+                cornerAnimator(drawable!!, initialCorner, finalCorner),
+                widthAnimator(this@CircularProgressButton2, initialWidth, finalWidth),
+                heightAnimator(this@CircularProgressButton2, initialHeight, finalHeight)
+            )
+
+            addListener(morphListener(::morphStart, ::morphEnd))
+        }
+    }
+
+    private val morphRevertAnimator by lazy {
+        AnimatorSet().apply {
+            playTogether(
+                cornerAnimator(drawable!!, finalCorner, initialCorner),
+                widthAnimator(this@CircularProgressButton2, finalWidth, initialWidth),
+                heightAnimator(this@CircularProgressButton2, finalHeight, initialHeight)
+            )
+
+            addListener(morphListener(::morphRevertStart, ::morphRevertEnd))
+        }
+    }
+
+    private fun morphStart() {
+        isClickable = false
+        waitingToStartProgress = false
+        text = null
+        state = State.MORPHING
+        setCompoundDrawables(null, null, null, null)
+    }
+
+    private fun morphEnd() {
+        isClickable = true
+
+        if (waitingToStartDone) {
+            waitingToStartDone = false
+
+            val runnable = Runnable { revealAnimatedDrawable?.start() }
+            Handler().postDelayed(runnable, 50)
+        }
+
+        state = State.PROGRESS
+    }
+
+    private fun morphRevertStart() {
+        isClickable = false
+        state = State.MORPHING
+    }
+
+    private fun morphRevertEnd() {
+        isClickable = true
+        state = State.IDLE
+
+        //        setCompoundDrawablesRelative(mParams.mDrawables[0], mParams.mDrawables[1], mParams.mDrawables[2], mParams.mDrawables[3])
+    }
+
+    fun startAnimation() {
+        if (state != State.IDLE) {
+            return
+        }
+
+        if (!viewReady) {
+            waitingToStartProgress = true
+            return
+        }
+
+        morphAnimator.start()
+    }
+
+    fun revertAnimation(onAnimationEndListener: () -> Unit = {}) {
+        if (state == State.IDLE) {
+            return
+        }
+
+        when (state) {
+            State.IDLE, State.MORPHING_REVERT -> return
+            State.MORPHING                    -> {
+                morphAnimator.end()
+            }
+            State.PROGRESS                    -> {
+                progressAnimatedDrawable?.stop()
+            }
+            else                              -> {
+            }
+        }
+
+
+        morphRevertAnimator.apply {
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    onAnimationEndListener()
+                }
+            })
+        }.start()
+    }
+
+
+    fun stopAnimation() {
+        state = State.STOPPED
+
+        if (state == State.PROGRESS) {
+            progressAnimatedDrawable?.stop()
+        }
+    }
+
+    fun doneLoadingAnimation(fillColor: Int, bitmap: Bitmap) {
+        when (state) {
+            State.PROGRESS -> {
+                progressAnimatedDrawable?.stop()
+                revealAnimatedDrawable?.start()
+            }
+            State.MORPHING -> {
+                waitingToStartDone = true
+            }
+        }
+
+        state = State.DONE
+    }
+
+    fun dispose() {
+        morphAnimator.disposeAnimator()
+        morphRevertAnimator.disposeAnimator()
+    }
+
+    private fun drawProgress(canvas: Canvas) {
+        progressAnimatedDrawable?.run {
+            if(isRunning) {
+                draw(canvas)
+            } else {
+                start()
+            }
+        }
+    }
+
+    private fun drawDoneAnimation(canvas: Canvas) {
+        revealAnimatedDrawable?.draw(canvas)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        viewReady = true
+
+        when (state) {
+            State.IDLE                            -> {
+                if (waitingToStartProgress) {
+                    morphAnimator.start()
+                }
+            }
+            State.MORPHING, State.MORPHING_REVERT -> {
+            }
+            State.PROGRESS                        -> drawProgress(canvas)
+            State.DONE                            -> drawDoneAnimation(canvas)
+
+        }
+    }
+}
+
+private fun parseGradientDrawable(drawable: Drawable): GradientDrawable =
+    when (drawable) {
+        is GradientDrawable  -> drawable
+        is ColorDrawable     -> GradientDrawable().apply { setColor(drawable.color) }
+        is InsetDrawable     -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                drawable.drawable?.let { innerDrawable ->
+                    parseGradientDrawable(innerDrawable)
+                }
+                    ?: throw RuntimeException("Error reading background... Use a shape or a color in xml!")
+            } else {
+                throw RuntimeException("Error reading background... Use a shape or a color in xml!")
+            }
+        }
+        is StateListDrawable -> {
+            if (drawable.current is GradientDrawable) {
+                drawable.current as GradientDrawable
+            } else {
+                throw RuntimeException("Error reading background... Use a shape or a color in xml!")
+            }
+        }
+        else                 -> throw RuntimeException("Error reading background... Use a shape or a color in xml!")
+
+    }
+
+
+private fun cornerAnimator(drawable: GradientDrawable, initial: Float, final: Float) =
+    ObjectAnimator.ofFloat(drawable, "cornerRadius", initial, final)
+
+private fun widthAnimator(view: View, initial: Int, final: Int) =
+    ValueAnimator.ofInt(initial, final).apply {
+        addUpdateListener { animation ->
+            val layoutParams = view.layoutParams
+            layoutParams.width = animation.animatedValue as Int
+            view.layoutParams = layoutParams
+        }
+    }
+
+private fun heightAnimator(view: View, initial: Int, final: Int) =
+    ValueAnimator.ofInt(initial, final).apply {
+        addUpdateListener { animation ->
+            val layoutParams = view.layoutParams
+            layoutParams.height = animation.animatedValue as Int
+            view.layoutParams = layoutParams
+        }
+    }
+
+private fun morphListener(morphStartFn: () -> Unit, morphEndFn: () -> Unit) =
+    object : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator?) {
+            morphEndFn()
+        }
+
+        override fun onAnimationStart(animation: Animator?) {
+            morphStartFn()
+        }
+    }
