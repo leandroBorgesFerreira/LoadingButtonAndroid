9730ea59360e8573b5a38a04e21fc84cbed16940
Leandro Borges Ferreira <lehen01@gmail.com>
2019-01-14 10:36:28 -0200

    making tests run it many buttons

--- app/src/androidTest/java/br/com/simplepass/loadingbutton/CircularProgressButtonTest.kt
+++ app/src/androidTest/java/br/com/simplepass/loadingbutton/CircularProgressButtonTest.kt
@@ -3,11 +3,12 @@ package br.com.simplepass.loadingbutton
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
@@ -25,29 +26,59 @@ class CircularProgressButtonTest {
 
     @JvmField
     @Rule
-    var activityTestRule = ActivityTestRule(MainActivity::class.java, true, true)
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, true, true)
+
+    //Buttons that doesn't revert the animation very fast
+    private val slowProgressButtons = arrayOf(
+        R.id.buttonTest1,
+        R.id.buttonTest2,
+        R.id.buttonTest6,
+        R.id.buttonTest7,
+        R.id.buttonTest8
+    )
+
+    private val allProgressButtons = arrayOf(
+        R.id.buttonTest1,
+        R.id.buttonTest2,
+        R.id.buttonTest3,
+        R.id.buttonTest4,
+        R.id.buttonTest5,
+        R.id.buttonTest6,
+        R.id.buttonTest7,
+        R.id.buttonTest8
+    )
+
+    private fun testButtonTextVisibilityBeforeAnimation(id: Int) {
+        onView(withId(id))
+            .perform(scrollTo())
+            .check(matches(allOf(isDisplayed(), isEnabled(), isClickable(), withText(R.string.send))))
+            .perform(click())
+            .check(matches(not(withText(R.string.send))))
+    }
+
+    private fun testButtonTextVisibilityBeforeAndAfterAnimation(id: Int) {
+        val progressButton: CircularProgressButton = activityTestRule.activity.findViewById(id)
+
+        onView(withId(id))
+            .perform(scrollTo())
+            .check(matches(allOf(isDisplayed(), isEnabled(), isClickable(), withText(R.string.send))))
+            .perform(click())
+
+        val progressButtonIdlingResource = ProgressButtonStateIdlingResource(progressButton)
+        IdlingRegistry.getInstance().register(progressButtonIdlingResource)
+
+        onView(withId(id)).check(matches(withText(R.string.send)))
+        IdlingRegistry.getInstance().unregister(progressButtonIdlingResource)
+    }
 
     @Test
     fun testTextVisibleBeforeSpinnerAnimation() {
-        onView(withId(R.id.buttonTest1))
-                .check(matches(allOf(isDisplayed(), isEnabled(), isClickable(), withText(R.string.send))))
-                .perform(click())
-                .check(matches(not(withText(R.string.send))))
+        slowProgressButtons.forEach(::testButtonTextVisibilityBeforeAnimation)
     }
 
     @Test
     fun testTextVisibleBeforeAndAfterSpinnerAnimation() {
-        val circularProgressButton = activityTestRule.activity.findViewById<CircularProgressButton>(R.id.buttonTest2)
-
-        onView(withId(R.id.buttonTest2))
-                .check(matches(allOf(isDisplayed(), isEnabled(), isClickable(), withText(R.string.send))))
-                .perform(click())
-
-        val progressButtonIdlingResource = ProgressButtonStateIdlingResource(circularProgressButton)
-        IdlingRegistry.getInstance().register(progressButtonIdlingResource)
-
-        onView(withId(R.id.buttonTest2)).check(matches(withText(R.string.send)))
-        IdlingRegistry.getInstance().unregister(progressButtonIdlingResource)
+        allProgressButtons.forEach(::testButtonTextVisibilityBeforeAndAfterAnimation)
     }
 
     @Test
