a582717cedbb3496428085dc658a0625aeb5e4cf
Leandro Ferreira <lehen01@gmail.com>
2020-04-23 17:11:11 +0200

    code rename

--- loading-button-android/src/main/java/br/com/simplepass/loadingbutton/Extensions.kt
+++ loading-button-android/src/main/java/br/com/simplepass/loadingbutton/Extensions.kt
@@ -11,13 +11,13 @@ internal fun Animator.disposeAnimator() {
 }
 
 internal fun View.updateWidth(width: Int) {
-    this.changeProperty { this.width = width }
+    this.changeLayoutProperty { this.width = width }
 }
 
 internal fun View.updateHeight(height: Int) {
-    this.changeProperty { this.height = height }
+    this.changeLayoutProperty { this.height = height }
 }
 
-private fun View.changeProperty(func: ViewGroup.LayoutParams.() -> Unit) {
+private fun View.changeLayoutProperty(func: ViewGroup.LayoutParams.() -> Unit) {
     this.layoutParams = this.layoutParams.apply(func)
 }
--- loading-button-android/src/main/java/br/com/simplepass/loadingbutton/customViews/ProgressButton.kt
+++ loading-button-android/src/main/java/br/com/simplepass/loadingbutton/customViews/ProgressButton.kt
@@ -25,6 +25,7 @@ import br.com.simplepass.loadingbutton.updateHeight
 import br.com.simplepass.loadingbutton.updateWidth
 import br.com.simplepass.loadingbutton.utils.addLifecycleObserver
 import br.com.simplepass.loadingbutton.utils.parseGradientDrawable
+import kotlin.math.abs
 
 interface ProgressButton : Drawable.Callback, LifecycleObserver {
     var paddingProgress: Float
@@ -155,7 +156,7 @@ internal fun ProgressButton.createRevealAnimatedDrawable(
     CircularRevealAnimatedDrawable(this, fillColor, bitmap).apply {
         val padding = Rect()
         drawableBackground.getPadding(padding)
-        val paddingSides = (Math.abs(padding.top - padding.left))
+        val paddingSides = (abs(padding.top - padding.left))
         setBounds(paddingSides, padding.top, finalWidth - paddingSides, finalHeight - padding.bottom)
         callback = this@createRevealAnimatedDrawable
     }
